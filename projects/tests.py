9f7952acd93f99718b254c77438f328a35e61615
Alfredo Carella <alfredocarella@gmail.com>
2020-07-24 17:29:45 +0200

    Add login requirement to project list- and detail views

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -6,6 +8,11 @@ from .models import Project
 
 class ProjectTests(TestCase):
     def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='newuser',
+            email='newuser@email.com',
+            password='testpass123'
+        )
         self.project = Project.objects.create(
             title="A moonshot",
             description="The Baltimore Gun Club wants shoot the Moon.",
@@ -18,16 +25,34 @@ class ProjectTests(TestCase):
         "The Baltimore Gun Club wants shoot the Moon.")
         self.assertEqual(f'{self.project.owner}', 'Alfredo C.')
 
-    def test_project_list_view(self):
+    def test_project_list_view_for_logged_in_user(self):
+        self.client.login(email='newuser@email.com', password='testpass123')
         response = self.client.get(reverse('project_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'A moonshot')
         self.assertTemplateUsed(response, 'projects/project_list.html')
 
-    def test_project_detail_view(self):
+    def test_project_list_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('project_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'{reverse("account_login")}?next=/projects/')
+        response = self.client.get(f'{reverse("account_login")}?next=/projects/')
+        self.assertContains(response, 'Logg inn')
+
+    def test_project_detail_view_for_logged_in_user(self):
+        self.client.login(email='newuser@email.com', password='testpass123')
         response = self.client.get(self.project.get_absolute_url())
         no_response = self.client.get('/projects/12345/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A moonshot')
         self.assertTemplateUsed(response, 'projects/project_detail.html')
+
+    def test_project_detail_view_for_logged_out_user(self):
+        self.client.logout()
+        response = self.client.get(self.project.get_absolute_url())
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f'{reverse("account_login")}?next={self.project.get_absolute_url()}')
+        response = self.client.get(f'{reverse("account_login")}?next={self.project.get_absolute_url()}')
+        self.assertContains(response, 'Logg inn')
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
+
 from .models import Project
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     context_object_name = 'project_list'
     template_name = 'projects/project_list.html'
+    login_url = 'account_login'
 
 
-class ProjectDetailView(DetailView):
+class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     context_object_name = 'project'
     template_name = 'projects/project_detail.html'
+    login_url = 'account_login'
