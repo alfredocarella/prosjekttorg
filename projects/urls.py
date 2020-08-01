55e621d5c7d9a70cc4d00ac2883d67ed2c37d1e9
Alfredo Carella <alfredocarella@gmail.com>
2020-08-02 01:38:58 +0200

    First draft for Project CRUD views

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ProjectListView, ProjectDetailView
+from .views import ProjectListView, ProjectDetailView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
 
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
+    path('create/', ProjectCreateView.as_view(), name='project_create'),
     path('<uuid:pk>', ProjectDetailView.as_view(), name='project_detail'),
+    path('<uuid:pk>/update', ProjectUpdateView.as_view(), name='project_update'),
+    path('<uuid:pk>/delete', ProjectDeleteView.as_view(), name='project_delete'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.http import Http404
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Project
 
@@ -16,3 +19,37 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'project'
     template_name = 'projects/project_detail.html'
     login_url = 'account_login'
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ['title', 'description', 'image', 'courses']
+    template_name = 'projects/project_create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(ProjectCreateView, self).form_valid(form)
+
+
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    fields = ['title', 'description', 'image', 'courses']
+    template_name = 'projects/project_update.html'
+
+    def get_object(self):
+        project = super(ProjectUpdateView, self).get_object()
+        if not project.user == self.request.user:
+            raise Http404
+        return project
+
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    template_name = 'projects/project_delete.html'
+    success_url = reverse_lazy('project_list')
+
+    def get_object(self):
+        project = super(ProjectDeleteView, self).get_object()
+        if not project.user == self.request.user:
+            raise Http404
+        return project
